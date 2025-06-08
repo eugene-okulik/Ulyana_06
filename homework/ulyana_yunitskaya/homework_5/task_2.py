8e69f135720e824b35d33c6b263b606e03a276dc
Ulya06 <top-32@mail.ru>
2025-06-08 19:52:43 +0200

    homework_5_ulyana_done

---
+++ homework/ulyana_yunitskaya/homework_5/task_2.py
@@ -0,0 +1,16 @@
+txt = 'результат операции: 42'
+# print(txt.index('42'))  # результат 20
+user_input = int(txt[20:])
+print(10 + user_input)
+
+
+txt_1 = 'результат операции: 514'
+# print(txt_1.index('514'))  # результат 20
+user_input_1 = int(txt_1[20:])
+print(10 + user_input_1)
+
+
+txt_2 = 'результат работы программы: 9'
+# print(txt_2.index('9'))  # результат 28
+user_input_2 = int(txt_2[28:])
+print(10 + user_input_2)
