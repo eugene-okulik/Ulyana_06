e1ca8f0589df4156db5e75cb9e52271dce1c9a7b
Ulya06 <top-32@mail.ru>
2025-06-09 10:13:37 +0200

    Updated ulyana_homework_5_task 2

--- homework/ulyana_yunitskaya/homework_5/task_2.py
+++ homework/ulyana_yunitskaya/homework_5/task_2.py
@@ -1,16 +1,18 @@
-txt = 'результат операции: 42'
-# print(txt.index('42'))  # результат 20
-user_input = int(txt[20:])
-print(10 + user_input)
+txt = 'результат операции: 52'
+colon_index = txt.index(':')
+label = txt[:colon_index]
+user_input = int(txt[colon_index + 1:]) + 10
+print(f'{label}: {user_input}')
+
+text = 'результат операции: 514'
+find_index = text.index(':')
+new = text[:find_index]
+user_input_1 = int(text[find_index + 1:]) + 10
+print(f'{new}: {user_input_1}')
 
 
-txt_1 = 'результат операции: 514'
-# print(txt_1.index('514'))  # результат 20
-user_input_1 = int(txt_1[20:])
-print(10 + user_input_1)
-
-
-txt_2 = 'результат работы программы: 9'
-# print(txt_2.index('9'))  # результат 28
-user_input_2 = int(txt_2[28:])
-print(10 + user_input_2)
+text_2 = 'результат работы программы: 9'
+find_index_2 = text_2.index(':')
+new_2 = text_2[:find_index_2]
+user_input_2 = int(text_2[find_index_2 + 1:]) + 10
+print(f'{new_2}: {user_input_2}')
