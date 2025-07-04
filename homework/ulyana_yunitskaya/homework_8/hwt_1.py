5c6beb2d44c16fea8a440a35b7d8866b37cd243c
Ulya06 <top-32@mail.ru>
2025-07-04 11:47:52 +0200

    homework_8_ulyana done

--- homework/ulyana_yunitskaya/homework_8/hwt_1.py
+++ homework/ulyana_yunitskaya/homework_8/hwt_1.py
@@ -5,6 +5,6 @@ bonus = random.choice([True, False])
 salary = int(input('What is your salary?'))
 
 if bonus is True:
-    print(f"{salary}, True - '${salary + random.randint(10, 1000)}'", sep =', ')
+    print(f"{salary}, True - '${salary + random.randint(10, 1000)}'", sep=', ')
 else:
-    print(f"{salary}, False - '${salary}'", sep =', ')
+    print(f"{salary}, False - '${salary}'", sep=', ')
