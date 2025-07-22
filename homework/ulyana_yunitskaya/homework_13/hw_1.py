a55095522bbf1bc1353bd755855204b6b07b032b
Ulya06 <top-32@mail.ru>
2025-07-22 17:13:54 +0200

    homework_13_ulyana done

---
+++ homework/ulyana_yunitskaya/homework_13/hw_1.py
@@ -0,0 +1,25 @@
+import os
+import datetime
+
+base_path = os.path.dirname(__file__)
+homework_path = os.path.dirname(os.path.dirname(base_path))
+eugene_file = os.path.join(homework_path, 'eugene_okulik', 'hw_13', 'data.txt')
+print(eugene_file)
+
+def date_split(line):
+    left, instruction = line.split(" - ", maxsplit=1)
+    number, date_str = left.split(" ", maxsplit=1)
+    return number, date_str, instruction
+
+with open(eugene_file, encoding='utf-8') as eugene_file_1:
+    for line in eugene_file_1:
+        number, date_str, instruction = date_split(line)
+        dt = datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S.%f")
+        if "на неделю позже" in instruction:
+            result = dt + datetime.timedelta(weeks=1)
+        elif "день недели" in instruction:
+            result = dt.strftime('%A')
+        elif "дней назад" in instruction:
+            result = f"{(datetime.datetime.now() - dt).days} дней назад"
+
+        print(f"{number} {result}")
