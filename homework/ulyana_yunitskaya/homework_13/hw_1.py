201802b9016f2c3c0205073ddbf613112fc34cbe
Ulya06 <top-32@mail.ru>
2025-07-22 17:16:41 +0200

    homework_13_ulyana done

--- homework/ulyana_yunitskaya/homework_13/hw_1.py
+++ homework/ulyana_yunitskaya/homework_13/hw_1.py
@@ -6,10 +6,12 @@ homework_path = os.path.dirname(os.path.dirname(base_path))
 eugene_file = os.path.join(homework_path, 'eugene_okulik', 'hw_13', 'data.txt')
 print(eugene_file)
 
+
 def date_split(line):
     left, instruction = line.split(" - ", maxsplit=1)
     number, date_str = left.split(" ", maxsplit=1)
     return number, date_str, instruction
+
 
 with open(eugene_file, encoding='utf-8') as eugene_file_1:
     for line in eugene_file_1:
