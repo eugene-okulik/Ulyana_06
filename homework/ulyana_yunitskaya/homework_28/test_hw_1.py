77330baa6b940b986c795bcb08fdbba2ce1bb466
Ulya06 <top-32@mail.ru>
2026-05-27 16:42:12 +0200

    update hw_1

--- homework/ulyana_yunitskaya/homework_28/test_hw_1.py
+++ homework/ulyana_yunitskaya/homework_28/test_hw_1.py
@@ -3,7 +3,8 @@ from playwright.sync_api import Page
 
 def test_form_authentication(page: Page):
     page.goto('https://the-internet.herokuapp.com/')
-    page.get_by_role('link',name='Form Authentication').click()
-    page.get_by_role('textbox',name='Username').fill('Ulyana')
-    page.get_by_role('textbox',name='Password').fill('123456')
-    page.get_by_role('button',name='Login').click()
+    page.get_by_role('link', name='Form Authentication').click()
+    page.get_by_role('textbox', name='Username').fill('Ulyana')
+    page.get_by_role('textbox', name='Password').fill('123456')
+    page.get_by_role('button', name='Login').click()
+
